526f259dbf826bb2a3a3fa8abb6758aa747b0caf
deciduously <github@deciduously.com>
2018-07-24 07:58:09 -0400

    Rust boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+extern crate actix;
+extern crate actix_web;
+#[macro_use]
+extern crate error_chain;
+#[macro_use]
+extern crate log;
+#[cfg(test)]
+extern crate pretty_assertions;
+extern crate pretty_env_logger;
+
+mod errors {
+    error_chain!{}
+}
+
+use errors::*;
+use std::env::{set_var, var};
+
+fn init_logging(level: u64) -> Result<()> {
+    let verbosity = match level {
+        0 => "warn",
+        1 => "info",
+        2 => "debug",
+        3 | _ => "trace",
+    };
+    if verbosity == "trace" {
+        set_var("RUST_BACKTRACE", "1");
+    };
+    set_var("RUST_LOG", verbosity);
+    pretty_env_logger::init();
+    info!(
+        "Set verbosity to {}",
+        var("RUST_LOG").chain_err(|| "Failed to set verbosity level")?
+    );
+    Ok(())
+}
+
+fn run() -> Result<()> {
+    init_logging(1)?;
+    info!("Hello, world!");
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(ref e) = run() {
+        error!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            debug!("caused by: {}", e);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            trace!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_it_compiles() {
+        assert!(true)
+    }
 }
